ca8b11a3a9e4810b19c2273ccf0e1ce68dc72b50
Andrew Barbarello <abarbs@bu.edu>
2012-12-07 19:10:09 -0500

    Rough example of how user-space ramdisk library might look/ be implemented

---
+++ src/rd_lib/rd.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/ioctl.h>
+#include "rd.h"
+#include "../ramdisk_module/ramdisk_module.h"
+
+static int rd_init(void);
+static int fd = -1;
+
+int rd_init()
+{
+  int rdfile = -1, retval = -1;
+  if (fd != -1)
+    return 0;
+  rdfile = open("/proc/ramdisk", 0);
+  if (rdfile == -1)
+    return -1;
+  retval = ioctl(rdfile, RD_INIT, NULL);
+  if (retval == 0 || errno == EALREADY)
+    fd = rdfile;
+  return retval;
+}
+
+int rd_creat(char *pathname)
+{
+  
+  if (rd_init() < 0)
+    return -1;
+
+  return 0;
+}
---
+++ src/rd_lib/test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "rd.h"
+
+int main() {
+  rd_creat("bananas");
+  printf("Finished\n");
+  return 0;
+}
