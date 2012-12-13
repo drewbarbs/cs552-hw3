ee3b36f6d7e858ea1270de013ae5b879fef8e164
Andrew Barbarello <abarbs@bu.edu>
2012-12-12 20:38:08 -0500

    Copied seulki's files

--- src/ramdisk_module/ramdisk_module.c
+++ src/ramdisk_module/ramdisk_module.c
@@ -40,13 +40,13 @@ static index_node_t *get_free_index_node(void);
 //static void release_index_node(index_node_t *inode);
 static index_node_t *get_parent_index_node(const char *pathname); // DOESNT TRASH PATHNAME
 static index_node_t *get_index_node(const char *pathname);
+static index_node_t *get_inode(size_t no);
 static void *get_free_data_block(void);
 static void release_data_block(void *data_block_ptr);
 static void release_all_data_blocks(index_node_t *index_node);
-static int add_directory_entry(index_node_t *parent_inode,
-				directory_entry_t new_entry); /* Insert directory entry,
-								 add increment size */
 static offset_info_t get_offset_info(index_node_t *inode, int offset);
+static directory_entry_t* get_directory_entry(index_node_t* inode, int index);
+
 /* Routines for implementing ramdisk API */
 static int rd_creat(const char *usr_str);
 static int rd_mkdir(const char *usr_str);
@@ -87,6 +87,7 @@ static super_block_t *super_block = NULL;
 static index_node_t *index_nodes = NULL; // 256 blocks/64 bytes per inode = 1024 inodes
 static void *block_bitmap = NULL; // 4 blocks => block_bitmap is 1024 bytes long
 static void *data_blocks = NULL; // len(data_blocks) == 7931 blocks
+static int temp = 0;
 static LIST_HEAD(file_descriptor_tables);
 
 
@@ -131,7 +132,7 @@ static int __init initialization_routine(void) {
     return 1;
   }
   proc_entry->proc_fops = &ramdisk_file_ops;
-
+  
   return 0;
 }
 
@@ -172,12 +173,23 @@ static int ramdisk_ioctl(struct inode *inode, struct file *filp,
     printk(KERN_ERR "Ramdisk called before being initialized\n");
     return -1;
   }
+  
+  int i;
 
   switch (cmd) {
   case RD_INIT:
-    return rd_init();
+    rd_init();
+    rd_mkdir("/hello");
+    rd_mkdir("/hello/world");
+    if (get_index_node("/hello/world")) printk("I found it\n");
+    rd_unlink("/hello/world");
+    if (get_index_node("/hello/world")) printk("I cannot found it\n");
+   break;
   case RD_CREAT:
     //
+    break;
+  case RD_MKDIR:
+    
     break;
   case DBG_PRINT_FDT_PIDS:
     debug_print_fdt_pids();
@@ -407,16 +419,18 @@ static index_node_t *get_free_index_node()
   spin_unlock(&super_block_spinlock);
   /* Look for an UNALLOCATED inode */
   for (i = 0; i < NUM_INODES; i++) {
-    p = (index_node_t *) ((void *) index_nodes + 64 * i);
-    if (write_trylock(&p->file_lock)) {
+    p = get_inode(i);
+    //if (write_trylock(&p->file_lock)) {
       if (p->type == UNALLOCATED) {
-	new_inode = p;
-	new_inode->type = ALLOCATED;
-	write_unlock(&new_inode->file_lock);
-	break;
-      } else
-	write_unlock(&p->file_lock);
-    }
+		  
+		new_inode = p;
+		new_inode->type = ALLOCATED;
+		//write_unlock(&new_inode->file_lock);
+		break;
+      } else {
+			//write_unlock(&p->file_lock);
+		}
+    //}
   }
   /* We should have been able to find such an inode */
   if (new_inode == NULL) {
@@ -434,9 +448,12 @@ static index_node_t *get_free_index_node()
 
 static index_node_t *get_index_node(const char *pathname)
 {
+  /*int i;
   const char *pathname_copy;
   char *token, *tokenize;
-  index_node_t *current = index_nodes, *parent = NULL;
+  //index_node_t *current = index_nodes, *parent = NULL;
+  index_node_t *curret = NULL;//index_nodes;
+  index_node_t *parent = NULL;
   if (strlen(pathname) == 0)
     return NULL;
   if (strlen(pathname) == 1 && pathname[0] == '/')
@@ -453,13 +470,47 @@ static index_node_t *get_index_node(const char *pathname)
     for (i = 0; i < current->size / sizeof(directory_entry_t); i++) {
       
     }
-
-    
   }
 
-  kfree(pathname_copy);
+  kfree(pathname_copy);*/
+  int i;
+  index_node_t *node = NULL;
+  
+  if (strlen(pathname) == 0)
+    return index_nodes;
+  
+  char *parent_pathname = (char *) kcalloc(strlen(pathname) + 1, sizeof(char), GFP_KERNEL);
+  const char *filename = strrchr(pathname, '/') + 1;
+  int filename_length = strlen(filename);
+  if (filename_length < MAX_FILE_NAME_LEN) {
+    strncpy(parent_pathname, pathname, strlen(pathname) - filename_length - 1);
+    index_node_t *parent_node = get_index_node(parent_pathname);
+    if (parent_node != NULL) {
+      for (i = 0; i < parent_node->size / DIR_ENTRY_SZ; ++i) {
+	    directory_entry_t *entry = get_directory_entry(parent_node, i);
+	    if (strncmp(entry->filename, filename, MAX_FILE_NAME_LEN) == 0) {
+		  node = get_inode(entry->index_node_number);
+		  break;
+		}
+	  }
+	}  
+  }
+  kfree(parent_pathname);
+  return node;
 }
 
+static directory_entry_t* get_directory_entry(index_node_t* inode, int index)
+{
+	if (inode->type != DIR || inode->size / DIR_ENTRY_SZ <= index)
+		return NULL;
+	
+	int direct_no = index / (BLK_SZ / DIR_ENTRY_SZ);
+	int entry_no = index % (BLK_SZ / DIR_ENTRY_SZ);
+	if (direct_no < DIRECT)
+		return (directory_entry_t *)(inode->direct[direct_no] + DIR_ENTRY_SZ * entry_no);
+	else
+		return NULL; // TODO: if direct_no is more than 8;
+}
 
 /*
  * Returns the index node of directory containing the file
@@ -470,12 +521,9 @@ static index_node_t *get_index_node(const char *pathname)
 static index_node_t *get_parent_index_node(const char *pathname)
 {
   char *pathname_copy;
-  char *token;
-  index_node_t *parent;
-  
+  index_node_t *parent = NULL;
   pathname_copy = (char *) kcalloc(strlen(pathname) + 1, sizeof(char), GFP_KERNEL);
   strncpy(pathname_copy, pathname, strlen(pathname) - strlen(strrchr(pathname, '/')));
-
   parent = get_index_node(pathname_copy);
   kfree(pathname_copy);
   return parent;
@@ -532,15 +580,6 @@ int rd_init()
   return 0;
 }
 
-/*
- * Returns the index node corresponding to the directory
- * containing the file indicated by pathname, or NULL
- * on error
- */
-static index_node_t *get_parent_index_node(char *pathname)
-{
-  return index_nodes; /*TODO */
-}
 static int rd_creat(const char *usr_str)
 {
   /* int status = 0, i = 0; */
@@ -575,37 +614,154 @@ static int rd_creat(const char *usr_str)
   return 0;
 }
 
-static int rd_mkdir(const char *usr_str)
+static int rd_mkdir(const char *pathname)
 {
-  /* index_node_t new_index_node = { */
-  /*   .type = DIR, */
-  /*   .size = 0, */
-  /*   .file_lock = RW_LOCK_UNLOCKED, */
-  /*   .direct = { NULL }; */
-  /*   .single_indirect = NULL, */
-  /*   .double_indirect = NULL */
-  /* }; */
-  /* directory_entry_t new_directory_entry = { */
-  /*   .filename = { '\0' }, */
-  /*   .index_node_number = 0 */
-  /* }; */
+  index_node_t new_index_node = {
+    .type = DIR,
+    .size = 0,
+    .file_lock = RW_LOCK_UNLOCKED,
+    .direct = { NULL },
+    .single_indirect = NULL,
+    .double_indirect = NULL
+  };
+  directory_entry_t new_directory_entry = {
+    .filename = { '\0' },
+    .index_node_number = 0
+  };
+  
+  
+/*  char new_path_name[MAX_FILE_NAME_LEN] = { '\0' };
+  size_t usr_str_len = strnlen_user(usr_str, MAX_FILE_NAME_LEN);
+  if (usr_str_len == 0 || !access_ok(VERIFY_READ, usr_str, MAX_FILE_NAME_LEN))
+    return -EINVAL; */
 
-  /* /\* Check input is valid *\/ */
-  /* pathname = copy_from_user(usr_str); */
-  /* parent = get_parent_index_node(pathname);   */
-  /* if (parent == NULL) */
-  /*   return -EINVAL; */
+  index_node_t *parent = get_parent_index_node(pathname);
+  if (parent == NULL)
+    return -EINVAL;
 
-  /* new_inode_ptr = get_free_index_node(); */
-  /* *new_inode_ptr = new_index_node; */
-  /* new_directory_entry.index_node_number = (new_inode_ptr - index_nodes) / INODE_SZ; */
-  /* strncpy(strrchr(pathname, '/'), &new_directory_entry.filename); */
-
-  /* write_lock(&new_inode_ptr->file_lock); */
-  /* add_directory_entry(parent, new_directory_entry); */
-  /* write_unlock(&new_inode->file_lock); */
+  index_node_t *new_inode_ptr = get_free_index_node();
+  if (new_inode_ptr == NULL)
+	return -EINVAL;
+  *new_inode_ptr = new_index_node;
+  directory_entry_t *entry;
+  if (parent->size % BLK_SZ == 0) {
+    entry = get_free_data_block();
+    parent->direct[parent->size / BLK_SZ] = entry;
+  } else {
+    entry = get_directory_entry(parent, parent->size / DIR_ENTRY_SZ - 1) + DIR_ENTRY_SZ;
+  }
+  entry->index_node_number = (new_inode_ptr - index_nodes) / INODE_SZ;
+	
+  strncpy(entry->filename, strrchr(pathname, '/') + 1, MAX_FILE_NAME_LEN);
+  parent->size += DIR_ENTRY_SZ;
+  
   return 0;
 }
+
+static void *get_free_data_block(void)
+{
+	//if (temp == 1) printk("temp == 1");
+	return data_blocks + (temp++) * BLK_SZ;
+}
+
+static void release_data_block(void *data_block_ptr)
+{
+	
+}
+
+static void release_all_data_blocks(index_node_t *index_node)
+{
+	
+}
+
+static index_node_t *get_inode(size_t index)
+{
+	return (index_node_t *)(index_nodes + INODE_SZ * index);
+}
+
+static int rd_unlink(const char *pathname)
+{	
+	int i;
+	index_node_t *parent_node = get_parent_index_node(pathname);
+    if (parent_node != NULL) {
+		int last_entry_index = parent_node->size / DIR_ENTRY_SZ - 1;
+		const char *filename = strrchr(pathname, '/');
+		for (i = 0; i <= last_entry_index; ++i) {
+			directory_entry_t *entry = get_directory_entry(parent_node, i);
+			if (strncmp(entry->filename, filename, MAX_FILE_NAME_LEN) == 0) {
+				index_node_t *node = get_inode(entry->index_node_number);
+				if (node->type == DIR) {
+					if (node->size != 0) return -EINVAL;
+				} else {
+					release_all_data_blocks(node);
+				}
+				
+				// Init node
+				memset(node, 0, INODE_SZ);
+				
+				// Delete Entry
+				directory_entry_t *last_entry = get_directory_entry(parent_node, last_entry_index);
+				if (entry != last_entry)
+					*entry = *last_entry;
+				parent_node->size -= DIR_ENTRY_SZ;
+				if (parent_node->size % BLK_SZ == 0) {
+					release_data_block(last_entry);
+					
+					// Remove last location(DIRECT)
+					if (parent_node->size / BLK_SZ < DIRECT) {
+						parent_node->direct[parent_node->size / BLK_SZ] = 0;
+					} else { // IF more than 8
+						// TODO
+					}
+				}
+				break;
+			}
+		}
+	}
+	
+	return 0;
+}
+
+static int rd_open(const pid_t pid, const char *pathname)
+{
+	index_node_t *node = get_index_node(pathname);
+	if (node == NULL)
+		return -EINVAL;
+	file_object_t new_fo = {
+		.index_node = node,
+		.file_position = 0
+	};
+		
+	file_descriptor_table_t *fdt = get_file_descriptor_table(pid);
+	return create_file_descriptor_table_entry(fdt, new_fo);
+}
+
+static int rd_close(const pid_t pid, const int fd)
+{
+	file_descriptor_table_t *fdt = get_file_descriptor_table(pid);
+	delete_file_descriptor_table_entry(fdt, fd);
+	return 0;
+}
+
+static int rd_readdir(const pid_t pid, const rd_readdir_arg_t *usr_arg)
+{
+	int i;
+	file_descriptor_table_t *fdt = get_file_descriptor_table(pid);
+	file_object_t fo = get_file_descriptor_table_entry(fdt, usr_arg->fd);
+	// if fo == null, return -1;
+	
+	if (fo->index_node->size == 0 || fo->index_node->size == fo->file_position)
+		return 0; 
+	
+	directory_entry_t *entry = get_directory_entry(fo->index_node, fo->file_position / DIR_ENTRY_SZ);
+	usr_arg->filename = entry->filename;
+	fo->file_position += DIR_ENTRY_SZ;
+	
+	set_file_descriptor_table_entry(fdt, usr_arg->fd, fo);
+	
+	return 1;
+}
+
 
 module_init(initialization_routine);
 module_exit(cleanup_routine);
--- src/rd_lib/test.c
+++ src/rd_lib/test.c
@@ -1,37 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "rd.h"
+//#include "rd.h"
+#include "../ramdisk_module/ramdisk_module.h"
 
 int main() {
-  /* char tokenize_me[] = {"/drew/documents/file"}; */
-  char *to_print = NULL;
-
-  char pathname[] = "/dir/doc/file";
-  const char *pathname_copy = pathname;
-  char *tokenize = pathname_copy + 1;
-  /* char *pathname_copy = calloc(strlen(pathname) + 1, sizeof(char)); */
-  /* strncpy(pathname_copy, pathname, strlen(pathname) - strlen(strrchr(pathname, '/'))); */
-  while ((to_print = strsep(&tokenize, "/")) != NULL) {
-      printf("%s\n", to_print);
-    }
-  //  printf("%s\n", pathname_copy);
-  printf("Finished\n");
-  return 0;
+	int rdfile = -1, retval = -1;
+  rdfile = open("/proc/ramdisk", 0);
+  if (rdfile == -1)
+    return -1;
+  retval = ioctl(rdfile, RD_INIT, NULL);
+  return retval;
 }
-
-
-for (i = 0; i < current->size / sizeof(directory_entry_t); i++) {
-  if (i % DIR_ENTRIES_PB == 0) {
-    if (i < DIRECT * DIR_ENTRIES_PB) {
-      block_ptr = current->direct[i/DIR_ENTRIES_PB];
-    }
-  }
-  /* if (block_ptr[i].filename is equal to token) {
-       current_index_node_number = block_ptr[i].index_node_number;
-       current
-     }
-
-
-  
- }
