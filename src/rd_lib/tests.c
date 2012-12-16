521b09a2dbc9fa98f8293632d085ce24e8d0f268
Andrew Barbarello <abarbs@bu.edu>
2012-12-16 14:50:46 -0500

    Added some tests

--- src/rd_lib/tests.c
+++ src/rd_lib/tests.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,9 +13,11 @@
 #define MAX_FILES 1023
 #define BLK_SZ 256		
 #define DIRECT 8		
-#define PTR_SZ 4		
+#define PTR_SZ 4
+#define DIR_ENTRY_SZ 16
 #define PTRS_PB  (BLK_SZ / PTR_SZ)
 #define MAX_FILE_SIZE (BLK_SZ * (DIRECT + PTRS_PB + PTRS_PB*PTRS_PB))
+#define NUM_DATA_BLOCKS 7931
 
 static char pathname[80];
 static char sequential_data[MAX_FILE_SIZE * 2];
@@ -55,8 +58,8 @@ int make_seqfile()
   }
 #endif  
 
-  if (rd_close(fd) < 0) {
-    fprintf (stderr, "rd_close: Reportedly wrote too much data! status: %d\n", 
+  if ((retval = rd_close(fd)) < 0) {
+    fprintf (stderr, "rd_close: Error closing seqfile! status: %d\n", 
 	     retval);
     exit(1);
   }
@@ -68,21 +71,26 @@ int make_seqfile()
 
 int main()
 {
-  int retval, fd, i, offset, index_node_number;
+  int retval, fd, i, offset, index_node_number, num_written;
   char comp;
 
+  /* Prepare data */
+  memset (pathname, 0, 80);
+  /* Fill data array where sequential_data[i] == "%d" i % 10 */
   for (i = 0; i < MAX_FILE_SIZE; i++) {
    sequential_data[i] = (char) (((int) '0') + i % 10);
   }
 
 #ifdef RAND_WRITE_AND_SEEK  
   make_seqfile();
-  if (retval = rd_open("/seqfile") < 0) {
+  if ((retval = rd_open("/seqfile")) < 0) {
     fprintf (stderr, "rd_open: Error opening seqfile! status: %d\n", 
 	     retval);
     exit(1);
   }
-  fd = retval;    
+  fd = retval;
+  /* Seek to random offsets in file, make sure the byte read is
+     equal to the offset */
   for (i = 0; i < 300; i++) {
     offset = rand() % (MAX_FILE_SIZE + 5);
     retval = rd_lseek(fd, offset);
@@ -106,8 +114,137 @@ int main()
      }
    }
 
+  memset(read_buf, 0, MAX_FILE_SIZE);
 
+  if ((retval = rd_close(fd)) < 0) {
+    fprintf (stderr, "rd_close: Reportedly wrote too much data! status: %d\n", 
+	     retval);
+    exit(1);
+  }
+
+  retval = rd_unlink("/seqfile");
+  if (retval < 0) {
+    fprintf(stderr, "rd_unlink: Error unlinking seqfile: status %d\n", retval);
+    exit(1);
+  }
+
+  /* Ensure that the root directory is now empty */
+  retval = rd_open("/");
+  if (retval < 0) {
+    fprintf(stderr, "rd_open: Error opening root file: status %d\n", retval);
+    exit(1);
+  }
+
+  fd = retval;
+  retval = rd_readdir(fd, read_buf);
+  if (retval != 0 || strlen(read_buf) > 0) {
+    fprintf(stderr, "rd_readdir: Expected empty root file, but got dir entry %s: status %d\n", read_buf, retval);
+    exit(1);
+  }
+  
+  if ((retval = rd_close(fd)) < 0) {
+    fprintf (stderr, "rd_close: Reportedly wrote too much data! status: %d\n", 
+	     retval);
+    exit(1);
+  }
+  
  #endif  
+
+#ifdef UNLINK
+  /* Tests that rd_unlink does not create fragmentation of directory entries.
+     
+     Assumes root directory is empty to begin with. Generates MAX_FILES (1023),
+     which utilizes DIRECT + 1 (indirect block) + 56 data blocks
+     a subset of them of size large enough to free a data_block,
+     so that remaining datablocks should be
+     NUM_DATA_BLOCKS - (DIRECT + 1 + 56 - 1)
+     and then tries to generate a file large enough to fill the remaining data
+     blocks 
+  */
+
+  memset (pathname, 0, 80);
+  /* Generate MAXIMUM regular files */
+  for (i = 0; i < MAX_FILES + 1; i++) { // go beyond the limit
+    sprintf (pathname, "/file%d", i);
+    
+    retval = rd_creat (pathname);
+    
+    if (retval < 0) {
+      fprintf (stderr, "rd_create: File creation error! status: %d\n", 
+	       retval);
+      
+      if (i != MAX_FILES)
+	exit (1);
+    }
+    
+    memset (pathname, 0, 80);
+  }   
+
+  /* Unlink enough  files to free a datablock + 1*/
+  for (i = 0; i < 15 + 2; i++) {
+    //    offset = rand() % MAX_FILES;
+    memset (pathname, 0, 80);
+    sprintf (pathname, "/file%d", i);
+    retval = rd_unlink(pathname);
+    if (retval < 0) {
+      fprintf(stderr, "rd_unlink: Error unlinking file%d: status %d\n", offset, retval);
+      exit(1);
+    }
+  }
+
+  retval = rd_creat("/bigfile1");
+  if (retval < 0) {
+    fprintf (stderr, "rd_create: Error creating /bigfile! status: %d\n",
+  	     retval);
+    exit(1);
+  }
+  
+  fd = rd_open("/bigfile1");
+  retval = rd_write(fd, sequential_data, MAX_FILE_SIZE);
+  if (retval != MAX_FILE_SIZE) {
+    fprintf (stderr, "rd_write: Reportedly wrote unexpected amount of data /bigfile1! status: %d\n",
+  	     retval);
+    exit(1);
+  }
+  num_written = retval;
+  if ((retval = rd_close(fd)) < 0) {
+    fprintf (stderr, "rd_close: Error closing /bigfile1! status: %d\n",
+  	     retval);
+    exit(1);
+  }
+  /* retval = rd_creat("/bigfile2"); */
+  /* if (retval < 0) { */
+  /*   fprintf (stderr, "rd_create: Error creating /bigfile2! status: %d\n", */
+  /* 	     retval); */
+  /*   exit(1); */
+  /* } */
+  
+  /* fd = rd_open("/bigfile2"); */
+  /* retval = rd_write(fd, sequential_data, MAX_FILE_SIZE); */
+  /* if (retval < 0) { */
+  /*   fprintf (stderr, "rd_write: Error writing to  /bigfile2! status: %d\n", */
+  /* 	     retval); */
+  /*   exit(1); */
+  /* } */
+  /* num_written += retval; */
+  /* if ((retval = rd_close(fd)) < 0) { */
+  /*   fprintf (stderr, "rd_close: Error closing /bigfile2! status: %d\n", */
+  /* 	     retval); */
+  /*   exit(1); */
+  /* } */
+  /* if (num_written != BLK_SZ * (NUM_DATA_BLOCKS - (DIRECT + 1 + 55) */
+  /* 			       - (1 + 1 + PTRS_PB))) { */
+  /*   fprintf (stderr, "rd_unlink: Error utilizing remaining data blocks! Amount of data written: %d\n", */
+  /* 	     num_written); */
+  /*   exit(1); */
+  /* } */
+
+
+  
+
+#endif
+
+
 
   fprintf(stdout, "Passed all of my own tests\n");
   return 0;
